3f78eef4922827a40b0171d87faa67eabca84831
Janeitor <53840214+Janeitor@users.noreply.github.com>
2026-07-21 21:15:57 -0400

    feat: close parking stay with exit time

--- src/main/java/cl/desafiolatam/parking/domain/model/ParkingStay.java
+++ src/main/java/cl/desafiolatam/parking/domain/model/ParkingStay.java
@@ -8,9 +8,18 @@ import cl.desafiolatam.parking.domain.exception.InvalidExitTimeException;
 public class ParkingStay {
 
     private final LocalDateTime entryTime;
+    private LocalDateTime exitTime;
 
     public ParkingStay(LocalDateTime entryTime) {
         this.entryTime = entryTime;
+    }
+
+    public void close(LocalDateTime exitTime) {
+        this.exitTime = exitTime;
+    }
+
+    public LocalDateTime getExitTime() {
+        return exitTime;
     }
 
     public long calculateDurationInMinutes(LocalDateTime exitTime) {
